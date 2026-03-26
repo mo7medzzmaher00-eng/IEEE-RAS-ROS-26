612f54c5c283e6691106108714ba79651b23fc93
Mohamed Maher Hassan <mo7medzzmaher00@gmail.com>
2026-03-26 22:46:57 +0200

    Implement max and min calculation for input numbers
    Added functionality to input numbers and calculate max and min.

--- Task_2/problem1.py
+++ Task_2/problem1.py
@@ -1,1 +1,13 @@
+import math 
 
+numbers=[]
+
+while  True:
+    num=int(input ("inter numbers (-1 will break):"))
+
+    if num is -1 :
+        break
+
+    numbers.append(num)
+print(f"max number is: {max(numbers)}")
+print(f"min number is: {min(numbers)}")
